7ff1f131a2d2f981a351bb440686eb2de620d292
Jahanzeb Hussain <jahanzebhussain@jahanzebslaptop.home>
2024-08-26 20:17:58 -0400

    Ui fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import { useColorMode } from '@docusaurus/theme-common'; // Import useColorMode hook
 
-
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -14,22 +13,22 @@ function HomepageHeader() {
   const { colorMode } = useColorMode();
 
   return (
-    <header className={styles.rugsafeHome}>
+    <header className={clsx(styles.rugsafeHome, colorMode === 'dark' && styles.darkMode)}>
       <div className={styles.heroContent}>
         <div className={styles.heroText}>
-          <h1 className={styles.heroTitle}>RugSafe</h1>
-          <h2 className={styles.heroSubtitle}>
+          <h1 className={clsx(styles.heroTitle, colorMode === 'dark' && styles.darkMode)}>RugSafe</h1>
+          <h2 className={clsx(styles.heroSubtitle, colorMode === 'dark' && styles.darkMode)}>
             The first secure vault for<br />crypto trades
           </h2>
-          <p className={styles.heroDescription}>
+          <p className={clsx(styles.heroDescription, colorMode === 'dark' && styles.darkMode)}>
             Rugsafe ensures your trades are secure by locking tokens in a
             vault, so you never have to worry about sudden rug pulls again.
           </p>
           <div className={styles.heroButtons}>
-            <Link className={styles.buttonPrimary} to="/docs/intro">
+            <Link className={clsx(styles.buttonPrimary, colorMode === 'dark' && styles.darkMode)} to="/docs/intro">
               Build whatever
             </Link>
-            <Link className={styles.buttonSecondary} to="/docs/intro">
+            <Link className={clsx(styles.buttonSecondary, colorMode === 'dark' && styles.darkMode)} to="/docs/intro">
               Introduction
             </Link>
           </div>
