88ed27253d3bf94aff3c19571143c0ad32707865
0xJ0 <4885245+Jovonni@users.noreply.github.com>
2025-01-04 18:26:21 -0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://docs.rugsafe.com',
-  baseUrl: '/RugSafeDocs', // was /
+  baseUrl: '/rugsafe-docs', // was /
 
   organizationName: 'rugsafe',
   projectName: 'rugsafedocs',
